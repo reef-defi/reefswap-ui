2388e68849dc9d20a1464dbb5410883a49ee3f12
Žiga Franko <franko.ziga@gmail.com>
2021-06-22 17:51:31 +0200

    Basic signer connection with polkadot extension

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  web3Accounts,
+  web3Enable,
+  web3FromAddress,
+  web3ListRpcProviders,
+  web3UseRpcProvider
+} from '@polkadot/extension-dapp';
+
+import { Keyring, WsProvider } from "@polkadot/api";
+import {
+  Provider,
+  Signer,
+  TestAccountSigningKey,
+} from "@reef-defi/evm-provider";
+
+import { TypeRegistry } from '@polkadot/types';
+
+const signUp = async (): Promise<void> => {
+  try {
+    const inj = await web3Enable("Reefswap");
+    console.log(inj);
+    const accounts = await web3Accounts();
+    const acc = accounts[0];
+    console.log(acc);
+
+    const keySigner = inj[0].signer;
+    console.log("Key singer: ", keySigner);
+
+    const provider =  new Provider({
+      provider: new WsProvider("wss://rpc-testnet.reefscan.com/ws")
+    });
+    await provider.api.isReady;
+    console.log("Provider connected!");
+
+    const wallet = new Signer(provider, acc.address, keySigner);
+    const address = await wallet.getAddress();
+    console.log(address);
+
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+}
+
 function App(): JSX.Element {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -22,6 +66,9 @@ function App(): JSX.Element {
         >
           Learn React
         </a>
+        <button className="btn btn-primary" onClick={() => signUp()}>
+          Connect
+        </button>
       </header>
     </div>
   );
