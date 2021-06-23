b38bbc315a444df46a13f3804dc3ae3faa22853e
Žiga Franko <franko.ziga@gmail.com>
2021-06-23 16:29:32 +0200

    nav is now responsive and has settings dropdown

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -14,6 +14,9 @@ const Button = ({to, name, selected}: Button): JSX.Element => (
 )
 
 const NavBar = (): JSX.Element => {
+  const accName = "Frenki-Account (Extension)";
+  const name = accName.length > 13 ? accName.slice(0, 12) + "..." + accName.slice(accName.length-4) : accName;
+  
   return (
     <nav className="container-fluid m-1 mt-3 row">
       <div className="col-md-4 col-sm-1 p-0"><img src="favicon.ico" alt="Not found!" /></div>
@@ -32,12 +35,26 @@ const NavBar = (): JSX.Element => {
             <div className="my-auto mx-2 fs-6 fw-bold">1000 REEF</div>
             <div className="btn-group">
               <button type="button" className="btn btn-secondary d-flex flex-column no-shadow nav-acc-button border-rad">
-                Frenki-Account (Extension)
+                {name}
               </button>
               <button type="button" className="btn btn-secondary dropdown-toggle dropdown-toggle-split no-shadow nav-acc-button border-rad" id="dropdownMenuReference" data-bs-toggle="dropdown" aria-expanded="false" data-bs-reference="parent">
                 <span className="visually-hidden">Toggle Dropdown</span>
               </button>
               <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuReference">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li><hr className="dropdown-divider"/></li>
+                <li><a className="dropdown-item" href="#">Separated link</a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="d-flex nav-acc border-rad">
+            <div className="dropdown">
+              <button className="btn btn-secondary d-flex flex-column no-shadow nav-acc-button border-rad" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                ...
+              </button>
+              <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                 <li><a className="dropdown-item" href="#">Action</a></li>
                 <li><a className="dropdown-item" href="#">Another action</a></li>
                 <li><a className="dropdown-item" href="#">Something else here</a></li>
