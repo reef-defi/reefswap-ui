550c31eec1008af804d33acd257f0997e0a6ee97
Žiga Franko <franko.ziga@gmail.com>
2021-06-23 14:33:33 +0200

    common card and loading updated

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import "./Card.css";
 
 const Card: React.FC<{}> = ({children}) => (
   <div className="card">
--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,14 +1,25 @@
 import React from "react"
 
 export const Loading = () =>  (
-  <div className="spinner-border text-secondary" role="status">
+  <div className="spinner-border text-secondary m-auto" role="status">
     <span className="visually-hidden">Loading...</span>
   </div>
 );
 
-export const LoadingPolkadotExtension = () => (
-  <div>
+interface LoadingWithText {
+  text: string;
+}
+
+export const LoadingWithText = ({text}: LoadingWithText) => (
+  <div className="d-flex flex-column justify-content-center">
     <Loading />
-    <span>Connecting to Polkadot extension...</span>
+    <span className="m-auto mt-2">{text}</span>
   </div>
 );
+
+export const LoadingComponent = ({text}: LoadingWithText) => (
+  <div className="container justify-content-center mt-4">
+    <LoadingWithText text={text} />
+  </div>
+)
+
