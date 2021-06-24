8cfb4a3a104e989d4ff93c58a2116d925ada1d7a
Žiga Franko <franko.ziga@gmail.com>
2021-06-24 12:21:45 +0200

    components added in content

--- src/pages/ContentController.tsx
+++ src/pages/ContentController.tsx
@@ -1,15 +1,19 @@
 import React from "react"
 import { Redirect, Route, Switch } from "react-router-dom";
-import { BIND_URL, POOL_URL, SWAP_URL } from "../urls";
+import { ADD_LIQUIDITY_URL, BIND_URL, IMPORT_POOL_URL, POOL_URL, SWAP_URL } from "../urls";
+import AddLiquidity from "./pool/AddLiquidity";
+import ImportPool from "./pool/ImportPool";
 import PoolContoller from "./pool/PoolContoller";
 import SwapController from "./swap/SwapController";
 
 const ContentController = () => (
   <Switch>
-    <Route exact path={SWAP_URL} component={SwapController} />
-    <Route exact path={POOL_URL} component={PoolContoller} />
     <Route exact path={BIND_URL} render={() => <div>Bind</div>} />
-    <Route path="/" render={() => <Redirect to={SWAP_URL} />} />
+    <Route exact path={POOL_URL} component={PoolContoller} />
+    <Route exact path={SWAP_URL} component={SwapController} />
+    <Route exact path={IMPORT_POOL_URL} component={ImportPool} />
+    <Route exact path={ADD_LIQUIDITY_URL} component={AddLiquidity} />
+    <Route path="/" render={() => <Redirect to={POOL_URL} />} />
   </Switch>
 );
 
