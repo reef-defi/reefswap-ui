7e1a7699e0651eabf8972169aa10e382b0f634ac
Žiga Franko <franko.ziga@gmail.com>
2021-06-24 10:28:03 +0200

    select token WIP component

---
+++ src/components/buttons/SelectToken.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Buttons.css";
+
+interface SelectTokenProps { }
+
+const SelectToken = ({} : SelectTokenProps) => {
+  return (
+    <>
+      <button type="button" className="btn btn-token-select border-1 border-rad" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Reef 
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down ms-1" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+        </svg>
+      </button>
+
+      <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              ...
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SelectToken;
