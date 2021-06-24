a50541300c9f2d7370834012470dbdb11fc9a819
Žiga Franko <franko.ziga@gmail.com>
2021-06-24 11:50:43 +0200

    Swap ui defined

--- src/pages/swap/SwapController.tsx
+++ src/pages/swap/SwapController.tsx
@@ -1,8 +1,9 @@
 import React, { useContext } from "react"
 import Card from "../../components/card/Card";
-import TokensManager from "../../components/buttons/SelectToken";
+
 import { AppContext } from "../../context/contexts";
 import "./SwapController.css";
+import SwapField from "./SwapField";
 
 interface SwapControllerProps {
 
@@ -14,8 +15,19 @@ const SwapController = ({} : SwapControllerProps) => {
   return (
     <Card>
       <h5 className="">Swap</h5>
-      <div className="swap-field p-3 border-rad">
-        <TokensManager />
+      <SwapField />
+      <div className="d-flex justify-content-center">
+        <div className="btn-swap-field border-rad">
+          <button className="btn btn-swap border-rad">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-down-short" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"/>
+            </svg>
+          </button>
+        </div>
+      </div>
+      <SwapField />
+      <div className="d-flex justify-content-center mt-2">
+        <button className="btn btn-reef w-25">Swap</button>
       </div>
     </Card>
   );
