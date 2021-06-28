fc4b928b1bacb9cd50684865f968ed909d68d475
Žiga Franko <franko.ziga@gmail.com>
2021-06-28 16:45:23 +0200

    token balance update action and reducer

--- src/store/reducers/tokens.ts
+++ src/store/reducers/tokens.ts
@@ -1,4 +1,4 @@
-import { TOKENS_ADD_TOKEN } from "../actionCreator";
+import { TOKENS_ADD_TOKEN, TOKENS_UPDATE_TOKEN_BALANCE } from "../actionCreator";
 import { Token, TokensAction } from "../actions/tokens";
 
 export interface TokensReducer {
@@ -7,12 +7,14 @@ export interface TokensReducer {
 
 export const defaultReefToken: Token = {
   name: "REEF", 
-  address: "0x0000000000000000000000000000000001000000"
+  address: "0x0000000000000000000000000000000001000000",
+  balance: "",
 };
 
 export const defaultRUSDToken: Token = {
   name: "RUSD",
-  address: "0x0000000000000000000000000000000001000001"
+  address: "0x0000000000000000000000000000000001000001",
+  balance: "",
 }
 
 const defaultTokensReducer: TokensReducer = {
@@ -22,6 +24,14 @@ const defaultTokensReducer: TokensReducer = {
 export const tokensReducer = (state=defaultTokensReducer, action: TokensAction): TokensReducer => {
   switch (action.type) {
     case TOKENS_ADD_TOKEN: return {...state, tokens: [...state.tokens, {...action.token}]};
+    case TOKENS_UPDATE_TOKEN_BALANCE:
+      return {...state,
+        tokens: [
+          ...state.tokens.slice(0, action.index), 
+          {...state.tokens[action.index], balance: action.balance},
+          ...state.tokens.slice(action.index+1),
+        ]
+      };
     default: return state;
   }
 }
