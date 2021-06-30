05d1c0137cab8b1eea0db45f40351f285b81ec63
Žiga Franko <franko.ziga@gmail.com>
2021-06-30 14:01:44 +0200

    Loading and updating tokens in app initialization

--- src/pages/AppInitialization.tsx
+++ src/pages/AppInitialization.tsx
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { web3Accounts, web3Enable } from '@polkadot/extension-dapp';
 import { WsProvider } from '@polkadot/api';
-import { Provider, Signer } from '@reef-defi/evm-provider';
+import { Provider } from '@reef-defi/evm-provider';
 import { LoadingWithText } from '../components/loading/Loading';
 import ErrorCard from '../components/error/ErrorCard';
 import ContentController from './ContentController';
 import { ReducerState } from '../store/reducers';
 import {
-  ReefswapSigner,
   utilsSetAccounts,
   utilsSetIsLoaded,
   utilsSetProvider,
   utilsSetSelectedAccount,
 } from '../store/actions/utils';
+import { accountsToSigners } from '../api/accounts';
+import { defaultTokenAddresses, loadTokens } from '../api/tokens';
+import { setAllTokens } from '../store/actions/tokens';
+import { ensure } from '../utils/utils';
 
 enum State {
   LOADING,
@@ -21,9 +24,12 @@ enum State {
   SUCCESS
 }
 
+
 const AppInitialization = (): JSX.Element => {
   const dispatch = useDispatch();
-  const { isLoaded } = useSelector((state: ReducerState) => state.utils);
+  const { tokens } = useSelector((state: ReducerState) => state.tokens);
+  const { isLoaded, selectedAccount, accounts } = useSelector((state: ReducerState) => state.utils);
+
   const [state, setState] = useState<State>(State.SUCCESS);
   const [error, setError] = useState('');
   const [status, setStatus] = useState('');
@@ -34,36 +40,33 @@ const AppInitialization = (): JSX.Element => {
         setState(State.LOADING);
         setStatus('Connecting to Polkadot extension...');
         const inj = await web3Enable('Reefswap');
-        if (inj.length === 0) {
-          throw new Error('Polkadot extension is disabled!');
-        }
+        ensure(inj.length > 0, "Polkadot extension is disabled! You need to approve the app in Polkadot-extension!");
+
         setStatus('Retrieving accounts...');
         const accounts = await web3Accounts();
+        ensure(accounts.length > 0, "To use Reefswap you need to create Polkadot account in Polkadot-extension!");
+
         setStatus('Connecting to chain...');
         const provider = new Provider({
           provider: new WsProvider('wss://rpc-testnet.reefscan.com/ws'),
         });
         await provider.api.isReadyOrError;
+        
         setStatus('Creating signers...');
-
-        const signers = await Promise.all(
-          accounts
-            .map((account) => ({
-              signer: new Signer(provider, account.address, inj[0].signer),
-              name: account.meta.name || '',
-            }))
-            .map(async (signer): Promise<ReefswapSigner> => ({
-              ...signer,
-              address: await signer.signer.getAddress(),
-            })),
+        const signers = await accountsToSigners(
+          accounts,
+          provider,
+          inj[0].signer
         );
 
+        setStatus("Loading tokens...");
+        const newTokens = await loadTokens(defaultTokenAddresses, signers[0].signer);
+        
+        dispatch(setAllTokens(newTokens));
         dispatch(utilsSetProvider(provider));
         dispatch(utilsSetAccounts(signers));
+        dispatch(utilsSetSelectedAccount(0));
         dispatch(utilsSetIsLoaded(true));
-        if (accounts.length > 0) {
-          dispatch(utilsSetSelectedAccount(0));
-        }
         setState(State.SUCCESS);
       } catch (e) {
         setError(e.message);
@@ -73,6 +76,28 @@ const AppInitialization = (): JSX.Element => {
 
     load();
   }, [dispatch]);
+
+  useEffect(() => {
+    // Just to make a not from runtime
+    // This function is called when the above useEffect is finished and reloads the tokens
+    // Be careful when modifying the code
+    const load = async (): Promise<void> => {
+      try {
+        setState(State.LOADING);
+        setStatus("Loading token balances...");
+        const {signer} = accounts[selectedAccount];
+        const addresses = tokens
+          .map((token) => token.address);
+        const newTokens = await loadTokens(addresses, signer);
+        dispatch(setAllTokens(newTokens));
+        setState(State.SUCCESS)
+      } catch (error) {
+        setError(error.message);
+        setState(State.ERROR);
+      }
+    };
+    load();
+  }, [selectedAccount, dispatch])
 
   return (
     <div className="container-fluid mt-4 w-100">
