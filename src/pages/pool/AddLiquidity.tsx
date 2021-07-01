d23d6c140035ec73d69ba94677e98bbbfdb04f80
Žiga Franko <franko.ziga@gmail.com>
2021-07-01 17:35:19 +0200

    AddLiquidity and ImportPool moved in pool folder

--- src/pages/pool/AddLiquidity.tsx
+++ src/pages/pool/AddLiquidity.tsx
@@ -1,29 +1,59 @@
 import React, { useState } from "react"
+import { toast } from 'react-toastify';
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { Token, TokenWithAmount } from "../../../api/tokens";
-import { CardWithBackTitle } from "../../../components/card/Card";
-import TokenAmountField from "../../../components/card/TokenAmountField";
-import { ReducerState } from "../../../store/reducers";
-import { POOL_URL } from "../../../utils/urls";
+import { addLiquidity, Token, TokenWithAmount } from "../../api/tokens";
+import { ButtonStatus } from "../../components/buttons/Button";
+import { CardWithBackTitle } from "../../components/card/Card";
+import TokenAmountField from "../../components/card/TokenAmountField";
+import { LoadingButtonIcon } from "../../components/loading/Loading";
+import { ReducerState } from "../../store/reducers";
+import { POOL_URL } from "../../utils/urls";
 
 interface AddLiquidityProps {
 
 }
 
+const buttonStatus = (token1: TokenWithAmount, token2: TokenWithAmount): ButtonStatus => {
+  if (token1.amount.length === 0) {
+    return { isValid: false, text: "Missing first token amount" };
+  } else if (token2.amount.length === 0) {
+    return { isValid: false, text: "Missing second token amount" };
+  } else {
+    return { isValid: true, text: "Add liquidity" };
+  }
+}
+
 const AddLiquidity = ({} : AddLiquidityProps) => {
   const {tokens} = useSelector((state: ReducerState) => state.tokens);
+  const {accounts, selectedAccount} = useSelector((state: ReducerState) => state.utils);
   const history = useHistory();
   const back = () => history.push(POOL_URL);
 
+  const [isLoading, setIsLoading] = useState(false);
   const [token1, setToken1] = useState<TokenWithAmount>({...tokens[0], amount: ""});
   const [token2, setToken2] = useState<TokenWithAmount>({...tokens[1], amount: ""});
+
+  const {text, isValid} = buttonStatus(token1, token2);
 
   const changeToken1 = (token: Token) => setToken1({...token, amount: ""});
   const changeToken2 = (token: Token) => setToken2({...token, amount: ""});
 
   const setAmount1 = (amount: string) => setToken1({...token1, amount});
   const setAmount2 = (amount: string) => setToken2({...token2, amount});
+
+  const addLiquidityClick = async () => {
+    try {
+      setIsLoading(true);
+      const {signer} = accounts[selectedAccount];
+      await addLiquidity(token1, token2, signer);
+      toast.success("Liquidity added successfully!");
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <CardWithBackTitle title="Add liquidity" onClick={back}>
@@ -33,6 +63,7 @@ const AddLiquidity = ({} : AddLiquidityProps) => {
       </div>
 
       <TokenAmountField
+        id="add-liquidity-token-1"
         token={token1}
         onTokenSelect={changeToken1}
         onAmountChange={setAmount1}
@@ -48,13 +79,18 @@ const AddLiquidity = ({} : AddLiquidityProps) => {
       </div>
 
       <TokenAmountField
+        id="add-liquidity-token-2"
         token={token2}
         onTokenSelect={changeToken2}
         onAmountChange={setAmount2}
       />
 
-      <button className="btn btn-reef border-rad w-100 mt-2">
-        Add liquidity
+      <button
+        className="btn btn-reef border-rad w-100 mt-2"
+        disabled={!isValid || isLoading}
+        onClick={addLiquidityClick}
+      >
+        {isLoading ? <LoadingButtonIcon /> : text}
       </button>
     </CardWithBackTitle>
   );
--- src/pages/pool/ImportPool.tsx
+++ src/pages/pool/ImportPool.tsx
@@ -1,8 +1,8 @@
 import React from "react"
 import { useHistory } from "react-router-dom";
-import SelectToken from "../../../components/buttons/SelectToken";
-import { CardWithBackTitle } from "../../../components/card/Card";
-import { POOL_URL } from "../../../utils/urls";
+import SelectToken from "../../components/buttons/SelectToken";
+import { CardWithBackTitle } from "../../components/card/Card";
+import { POOL_URL } from "../../utils/urls";
 
 interface ImportPoolProps {
 
