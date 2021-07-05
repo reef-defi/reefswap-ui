45d32d0a5b1a756cc16e89c438412ad2a6105c1c
Žiga Franko <franko.ziga@gmail.com>
2021-07-05 18:17:06 +0200

    is signer in pool function and structure refactored

--- src/api/pools.ts
+++ src/api/pools.ts
@@ -4,12 +4,16 @@ import { Token, TokenWithAmount } from "./tokens";
 import { Contract } from "ethers";
 import { ReefswapERC20 } from "../assets/abi/ReefswapERC20";
 import BN from "bn.js";
+import { ensure } from "../utils/utils";
+
+const EMPTY_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 interface ReefswapPoolBase {
   address: string;
   token1: TokenWithAmount;
   token2: TokenWithAmount;
 }
+
 
 export interface ReefswapPool extends ReefswapPoolBase {
   signerBalance: BN;
@@ -27,8 +31,9 @@ const uniqueCombinations = <T,>(array: T[]): [T, T][] => {
 }
 
 
-const tokensToPairAddresses = async (tokenCombinations: [Token, Token][], reefswapFactory: Contract): Promise<ReefswapPoolBase[]> => 
-  Promise.all(
+const tokensToPairAddresses = async (tokenCombinations: [Token, Token][], signer: Signer): Promise<ReefswapPoolBase[]> => {
+  const reefswapFactory = getReefswapFactory(signer);
+  return Promise.all(
     tokenCombinations
       .map(async ([token1, token2]): Promise<ReefswapPoolBase> => ({
         token1: {...token1, amount: ""},
@@ -37,26 +42,48 @@ const tokensToPairAddresses = async (tokenCombinations: [Token, Token][], reefsw
       })
     )
   );
+}
+
+const poolContract = async (token1: Token, token2: Token, signer: Signer): Promise<ReefswapPool> => {
+  const [subPool] = await tokensToPairAddresses([[token1, token2]], signer);
+  ensure(subPool.address !== EMPTY_ADDRESS, "Pool does not exist!");
+  const contract = new Contract(subPool.address, ReefswapERC20, signer);
+  const amount = await contract.balanceOf(await signer.getAddress());
+  const signerBalance = new BN(amount.toString());
+  return {...subPool, contract, signerBalance}
+}
+
+const ensurePoolBalance = async (pool: ReefswapPool, signer: Signer): Promise<void> => {
+  const signerAddress = await signer.getAddress();
+  const amount = await pool.contract.balanceOf(signerAddress);
+  const balance = new BN(amount.toString());
+  ensure(balance.gt(new BN("0")), "Signer not in pool!");
+}
+
+export const isSignerInPool = async (token1: Token, token2: Token, signer: Signer): Promise<boolean> => {
+  try {
+    const pool = await poolContract(token1, token2, signer);
+    await ensurePoolBalance(pool, signer);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
 
 export const loadPools = async (tokens: Token[], signer: Signer): Promise<ReefswapPool[]> => {
   const tokenCombinations = uniqueCombinations(tokens);
-  const reefswapFactory = getReefswapFactory(signer);
 
-  const signerAddress = await signer.getAddress();
-  const allReefswapPoolContracts = await tokensToPairAddresses(tokenCombinations, reefswapFactory);
-
-  const pools: ReefswapPool[] = await Promise.all(
-    allReefswapPoolContracts
-      .filter((contract) =>
-        contract.address !== "0x0000000000000000000000000000000000000000")
-      .map((pool) =>
-        ({...pool, contract: new Contract(pool.address, ReefswapERC20, signer) }))
-      .map(async (pool) =>
-        ({...pool, signerBalance: new BN((await pool.contract.balanceOf(signerAddress)).toString())}))
-  );
-
-  const existingPools = pools
-    .filter((pool) => pool.signerBalance.gt(new BN(0)))
-  return existingPools;  
+  let pools: ReefswapPool[] = [];
+  for (let index = 0; index < tokenCombinations.length; index ++) {
+    try {
+      const [token1, token2] = tokenCombinations[index];
+      const pool = await poolContract(token1, token2, signer);
+      await ensurePoolBalance(pool, signer);
+      pools.push(pool);
+    } catch (e) {
+      continue;
+    }
+  }
+  return pools;  
 }
 
