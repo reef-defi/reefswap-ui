4e2603ff6b5fc71f3d430b64ae4e7129b104f977
Žiga Franko <franko.ziga@gmail.com>
2021-07-15 16:58:36 +0200

    error toast and messages update

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,23 +1,20 @@
+import { toast } from "react-toastify";
+
 const chainErrors: {[key: string]: string} = {
   "INVALID_TO": "Invalid to address",
   "PAIR_EXISTS": "Pool pair already exist",
   "ZERO_ADDRESS": "Zero address  was passed",
   "IDENTICAL_ADDRESSES": "Equal addresses picked",
   "INSUFFICIENT_A_AMOUNT": "Insufficient first token amount",
-  "INSUFFICIENT_B_AMOUNT": "Insufficient first token amount",
+  "INSUFFICIENT_B_AMOUNT": "Insufficient second token amount",
   "INSUFFICIENT_LIQUIDITY": "Insufficient liquidity",
-  "INSUFFICIENT_INPUT_AMOUNT": "Insufficient input amount",
-  "INSUFFICIENT_OUTPUT_AMOUNT": "Insufficient output amount",
+  "INSUFFICIENT_INPUT_AMOUNT": "Insufficient sell amount",
+  "INSUFFICIENT_OUTPUT_AMOUNT": "Insufficient buy amount",
   "INSUFFICIENT_LIQUIDITY_MINTED": "Insufficient liquidity minted",
   "INSUFFICIENT_LIQUIDITY_BURNED": "Insufficient liquidity burned",
 }
 
-const errorHandler = (error: any): string => {
-  if (error.message) {
-    return error.message;
-  }
-  const message: string = error;
-
+const errorHandler = (message: string): string => {
   const errorKey = Object
     .keys(chainErrors)
     .find((errorKey) => message.includes(errorKey));
@@ -28,4 +25,9 @@ const errorHandler = (error: any): string => {
 
   return chainErrors[errorKey];
 }
+
+export const errorToast  = (message: string): void => {
+  toast.error(errorHandler(message));
+}
+
 export default errorHandler;
