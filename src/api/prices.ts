52dec60c72edad0f3d9c2a5a6c1a34e8a146d9d2
Žiga Franko <franko.ziga@gmail.com>
2021-08-17 18:45:36 +0200

    token price api connects to coingecko

---
+++ src/api/prices.ts
@@ -0,0 +1,16 @@
+import axios, {AxiosResponse} from "axios";
+
+interface PriceRes {
+  [currenty: string]: {
+    usd: number;
+  }
+}
+
+const coingeckoApi = axios.create({
+  baseURL: "https://api.coingecko.com/api/v3/",
+});
+
+export const getTokenPrice = async (tokenId: string): Promise<number> => coingeckoApi
+  .get<void, AxiosResponse<PriceRes>>(`/simple/price?ids=${tokenId}&vs_currencies=usd`)
+  .then((res) => res.data[tokenId].usd);
+  
