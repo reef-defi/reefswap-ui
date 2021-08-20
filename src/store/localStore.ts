cf8022889eda94f0e680b614b6be17819b09edaf
Žiga Franko <franko.ziga@gmail.com>
2021-08-20 17:08:34 +0200

    local storage update

--- src/store/localStore.ts
+++ src/store/localStore.ts
@@ -1,11 +1,10 @@
-
-const SIGNER_POINTER = "signer-pointer";
+const SIGNER_POINTER = 'signer-pointer';
 
 export const saveSignerPointer = (index: number): void => {
-  localStorage.setItem(SIGNER_POINTER, index + "");
-}
+  localStorage.setItem(SIGNER_POINTER, `${index}`);
+};
 
 export const getSignerPointer = (): number => {
-  const item = localStorage.getItem(SIGNER_POINTER)
-  return item ? parseInt(item) : 0;
-}
+  const item = localStorage.getItem(SIGNER_POINTER);
+  return item ? parseInt(item, 10) : 0;
+};
