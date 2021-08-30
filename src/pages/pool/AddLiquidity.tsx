4520725227d9ad7f7d997eb24f5b368862eb85a6
Žiga Franko <franko.ziga@gmail.com>
2021-08-30 14:48:35 +0200

    add liquidity update

--- src/pages/pool/AddLiquidity.tsx
+++ src/pages/pool/AddLiquidity.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { useHistory } from 'react-router-dom';
 import { ButtonStatus } from '../../components/buttons/Button';
@@ -13,22 +13,26 @@ import { setPools } from '../../store/actions/pools';
 import { defaultGasLimit } from '../../store/internalStore';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { errorToast } from '../../utils/errorHandler';
-import { loadPools } from '../../api/rpc/pools';
+import { loadPools, poolContract } from '../../api/rpc/pools';
 import {
-  TokenWithAmount, addLiquidity, loadTokens,
+  TokenWithAmount, addLiquidity, loadTokens, createEmptyTokenWithAmount, toTokenAmount,
 } from '../../api/rpc/tokens';
-import { PriceHook } from '../../hooks/priceHook';
-import { calculateCurrencyAmount } from '../../utils/math';
+import { retrieveReefCoingeckoPrice } from '../../api/prices';
+import { poolRatio } from '../../utils/math';
 
 const buttonStatus = (token1: TokenWithAmount, token2: TokenWithAmount, isEvmClaimed: boolean): ButtonStatus => {
   if (!isEvmClaimed) {
     return { isValid: false, text: 'Bind account' };
+  } if (token1.isEmpty) {
+    return { isValid: false, text: 'Select first token'};
+  } if (token2.isEmpty) {
+    return { isValid: false, text: 'Select second token'};
   } if (token1.amount.length === 0) {
     return { isValid: false, text: 'Missing first token amount' };
   } if (token2.amount.length === 0) {
     return { isValid: false, text: 'Missing second token amount' };
   }
-  return { isValid: true, text: 'Add liquidity' };
+  return { isValid: true, text: 'Supply' };
 };
 
 const AddLiquidity = (): JSX.Element => {
@@ -40,23 +44,73 @@ const AddLiquidity = (): JSX.Element => {
 
   const [isLiquidityLoading, setIsLiquidityLoading] = useState(false);
   const [gasLimit, setGasLimit] = useState(defaultGasLimit());
-  const [token1, isToken1Loading, setToken1] = PriceHook(0);
-  const [token2, isToken2Loading, setToken2] = PriceHook(1);
 
-  const isLoading = isLiquidityLoading || isToken1Loading || isToken2Loading;
+  const [isPriceLoading, setIsPriceLoading] = useState(false);
+  const [token2, setToken2] = useState(createEmptyTokenWithAmount());
+  const [token1, setToken1] = useState(toTokenAmount(tokens[0], {amount: "", price: 0, index: 0}));
+
+  const isLoading = isLiquidityLoading || isPriceLoading;
   const { signer, isEvmClaimed } = accounts[selectedAccount];
   const { text, isValid } = buttonStatus(token1, token2, isEvmClaimed);
 
+  useEffect(() => {
+    const load = async () => {
+      if (token1.isEmpty ||token2.isEmpty) { return; }
+      try {
+        setIsPriceLoading(true);
+        const reefPrice = await retrieveReefCoingeckoPrice();
+        const basePool = await poolContract(token1, token2, signer, settings);
+        const baseRatio = poolRatio(basePool);
+
+        if (token1.name === "REEF") {
+          setToken1({...token1, price: reefPrice});
+          setToken2({...token2, price: reefPrice/baseRatio});
+        } else if (token2.name === "REEF") {
+          setToken1({...token1, price: reefPrice*baseRatio});
+          setToken2({...token2, price: reefPrice});
+        } else {
+          const sellPool = await poolContract(tokens[0], token1, signer, settings);
+          const sellRatio = poolRatio(sellPool);
+          setToken1({...token1, price: reefPrice/sellRatio});
+          setToken2({...token2, price: reefPrice/sellRatio*baseRatio});
+        }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setIsPriceLoading(false);
+      }
+    };
+    load();
+  }, [token1.address, token2.address]);
+
   const back = (): void => history.push(POOL_URL);
   const changeToken1 = (index: number): void => setToken1({
-    ...tokens[index], amount: '', price: 0, index,
+    ...tokens[index], amount: '', price: 0, index, isEmpty: false,
   });
   const changeToken2 = (index: number): void => setToken2({
-    ...tokens[index], amount: '', price: 0, index,
+    ...tokens[index], amount: '', price: 0, index, isEmpty: false,
   });
 
-  const setAmount1 = (amount: string): void => setToken1({ ...token1, amount });
-  const setAmount2 = (amount: string): void => setToken2({ ...token2, amount });
+  const setAmount1 = (amount: string): void => {
+    // if (amount === "") {
+    //   setToken1({...token1, amount: ''});
+    //   setToken2({...token2, amount: ''});
+    // } else {
+      // const amo = parseFloat(amount)*token1.price/token2.price;
+      setToken1({ ...token1, amount });
+    //   setToken2({...token2, amount: `${amo.toFixed(4)}`});
+    // }
+  };
+  const setAmount2 = (amount: string): void => {
+    // if (amount === "") {
+    //   setToken1({...token1, amount: ''});
+    //   setToken2({...token2, amount: ''});
+    // } else {
+    //   const amo = parseFloat(amount)*token2.price/token1.price;
+    //   setToken1({...token1, amount: `${amo.toFixed(4)}`});
+      setToken2({ ...token2, amount });
+    // }
+  };
 
   const addLiquidityClick = async (): Promise<void> => {
     try {
