e3a46bdce09aaa7b030f14065cde8ee09ef18957
Žiga Franko <franko.ziga@gmail.com>
2021-08-31 16:56:19 +0200

    settings UX

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { DEFAULT_SLIPPAGE_TOLERANCE, MAX_SLIPPAGE_TOLERANCE, Settings } from '../../store/internalStore';
 import './Card.css';
 import { BackIcon, GearIcon } from './Icons';
+import ReactTooltip from 'react-tooltip';
 
 const Card: React.FC = ({ children }): JSX.Element => (
   <div className="card border-rad">
@@ -40,31 +42,70 @@ export const CardBack = ({ onBack }: CardBack): JSX.Element => (
   </button>
 );
 
-interface Settings {
-  gasLimit: string;
-  setGasLimit: (value: string) => void;
-}
 interface CardSettings {
   id?: string;
   settings: Settings;
+  setSettings: (value: Settings) => void;
 }
 
-export const CardSettings: React.FC<CardSettings> = ({ settings, id = 'settings' }) => (
+export const CardSettings: React.FC<CardSettings> = ({ settings, setSettings, id = 'settings' }) => (
   <div className="btn-group">
     <button className="btn" type="button" id={id} data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
       <GearIcon />
     </button>
-    <div className="dropdown-menu dropdown-menu-end" aria-labelledby={id} style={{ minWidth: '300px' }}>
-      <div className="m-3 d-flex flex-column">
-        <label className="ms-2 form-label" htmlFor="gas-limit">Gas limit</label>
-        <input
-          min="0"
-          type="number"
-          id="gas-limit"
-          value={settings.gasLimit}
-          className="form-control field-input"
-          onChange={(event) => settings.setGasLimit(event.target.value)}
-        />
+    <div className="dropdown-menu dropdown-menu-end border-rad" aria-labelledby={id} style={{ minWidth: '300px' }}>
+      <div className="m-3">
+        <h5 className="lead-text text-muted">Transaction settings</h5>
+        <label className="form-label text-muted sub-text" htmlFor="slipping-tolerance">
+          Slippage tolerance
+          <b className="ms-1" data-tip data-for="slipping-tolerance-tooltip">?</b>
+          <ReactTooltip id="slipping-tolerance-tooltip" place="bottom" effect="solid" backgroundColor="#46288b">
+            Your transaction will revert if <br/> the price changes<br/>unfacorably by more than<br/>this percentage.
+          </ReactTooltip>
+        </label>
+        <div className="d-flex flex-row">
+          <button
+            className={`btn ${settings.percentage === DEFAULT_SLIPPAGE_TOLERANCE ? "btn-reef" : "btn-secondary"} border-rad me-1`}
+            onClick={() => setSettings({...settings, percentage: DEFAULT_SLIPPAGE_TOLERANCE})}
+          >Auto</button>
+          <div className="input-group">
+            <input
+              min={0}
+              max={100}
+              step={0.1}
+              type="number"
+              id="slipping-tolerance"
+              value={settings.percentage}
+              className="form-control field-input text-end border-rad border-right-0"
+              onChange={(event) => setSettings({...settings, percentage: parseFloat(event.target.value)})}
+            />
+            <span className="input-group-text field-input border-rad ps-1">%</span>
+          </div>
+        </div>
+        <div className="d-flex">
+          {settings.percentage < DEFAULT_SLIPPAGE_TOLERANCE && <span className="text-war sub-text mx-auto">Your transaction may fail</span>}
+          {settings.percentage > MAX_SLIPPAGE_TOLERANCE && <span className="text-war sub-text mx-auto">Your transaction may be frontrun</span>}
+        </div>
+        <label className="form-label text-muted sub-text mt-2" htmlFor="deadline">
+          Deadline
+          <b className="ms-1" data-tip data-for="deadline-tooltip">?</b>
+          <ReactTooltip id="deadline-tooltip" place="bottom" effect="solid" backgroundColor="#46288b">
+            Your transaction will revert if<br/>it is pending for more than<br/>this period or time.
+          </ReactTooltip>
+        </label>
+        <div className="d-flex flex-row">
+          <input
+            min={1}
+            max={30}
+            step={1}
+            type="number"
+            id="deadline"
+            value={settings.deadline}
+            className="form-control field-input border-rad w-50 text-end"
+            onChange={(event) => setSettings({...settings, deadline: parseInt(event.target.value)})}
+          />
+          <span className="my-auto ms-1 text-muted sub-text">Minutes</span>
+        </div>
       </div>
     </div>
   </div>
