a73c8f53d5d0ed62deea4e939f77512eb7839077
Žiga Franko <franko.ziga@gmail.com>
2021-08-31 13:11:38 +0200

    Account inline info

---
+++ src/components/navigation/AccountInlineInfo.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Identicon from '@polkadot/react-identicon';
+import { trim } from '../../utils/utils';
+
+interface AccountInlineInfo {
+  name: string;
+  address: string;
+  evmAddress: string;
+  toggle?: string;
+  onClick: () => void;
+}
+
+const AccountInlineInfo = ({
+  name, address, evmAddress, toggle="exampleModalToggle", onClick,
+} : AccountInlineInfo): JSX.Element => (
+  <div className="d-flex flex-row px-0">
+    <div className="my-auto mx-2 rounded-circle bg-white">
+      <Identicon
+        value={address}
+        size={32}
+        theme="substrate"
+      />
+    </div>
+    <div className="d-flex flex-column align-start ps-2 pe-4" onClick={onClick} role="button" tabIndex={0} data-bs-target={`#${toggle}`} data-bs-toggle="modal" data-bs-dismiss="modal">
+      <span className="lead-text">{trim(name, 30)}</span>
+      <span className="sub-text">{trim(evmAddress, 30)}</span>
+    </div>
+  </div>
+);
+
+export default AccountInlineInfo;
