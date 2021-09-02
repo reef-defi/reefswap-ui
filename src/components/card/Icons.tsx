ab5b6c05b267027e1cbb86f9f81011156e2d8d3f
Žiga Franko <franko.ziga@gmail.com>
2021-09-02 19:25:01 +0200

    new icons

--- src/components/card/Icons.tsx
+++ src/components/card/Icons.tsx
@@ -11,7 +11,7 @@ interface Icon extends BaseIcon {
 const Icon: React.FC<Icon> = ({ small = false, className = '', children }): JSX.Element => {
   const size = small ? '16' : '20';
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="currentColor" className={`bi my-auto ${className}`} viewBox="0 0 16 16">
+    <svg width={size} height={size} fill="currentColor" className={`bi my-auto ${className}`} viewBox="0 0 16 16">
       { children }
     </svg>
   );
@@ -94,3 +94,23 @@ export const PlusIcon = ({ small } : BaseIcon): JSX.Element => (
     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
   </Icon>
 );
+
+const DefaultTokenIcon = ({small}: BaseIcon): JSX.Element => (
+  <svg fill="currentColor" className="select-token-icon-size" viewBox="0 0 16 16">
+    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+    <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
+  </svg>
+)
+
+interface TokenIcon extends BaseIcon {
+  src: string;
+}
+
+export const TokenIcon = ({src, small}: TokenIcon): JSX.Element => {
+  if (src === "") {
+    return (<DefaultTokenIcon small={small} />);
+  }
+  return (
+    <img src={src} className="select-token-icon-size rounded-circle" alt="..." />
+  )
+};
