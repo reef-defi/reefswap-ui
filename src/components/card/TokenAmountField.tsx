e29563ca240415454383d506831f2b0282ff81b3
Žiga Franko <franko.ziga@gmail.com>
2021-09-05 23:31:42 +0200

    token amount refactored -> future compression needed

--- src/components/card/TokenAmountField.tsx
+++ src/components/card/TokenAmountField.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import SelectToken from '../buttons/SelectToken';
-// import { showBalance } from '../../utils/math';
 import { Token, TokenWithAmount } from '../../api/rpc/tokens';
-import { showBalance } from '../../utils/math';
+import { showBalance, toBalance } from '../../utils/math';
+import { InputAmount } from '../inputs/Inputs';
+
+
 
 interface TokenAmountFieldProps {
   id?: string;
@@ -12,13 +14,12 @@ interface TokenAmountFieldProps {
   placeholder?: string;
 }
 
-const TokenAmountField = ({
-  id = 'exampleModal', token, onTokenSelect, onAmountChange, placeholder = '0,0',
-} : TokenAmountFieldProps): JSX.Element => {
+const TokenAmountFieldBase: React.FC<TokenAmountFieldProps> = ({
+  id = 'exampleModal', token, onTokenSelect, onAmountChange, placeholder = '0,0', children,
+}): JSX.Element => {
   const {
-    name, isEmpty, amount, price, iconUrl,
+    name, isEmpty, amount, iconUrl,
   } = token;
-  const amo = parseFloat(amount);
 
   return (
     <div className="field p-3 border-rad">
@@ -29,26 +30,103 @@ const TokenAmountField = ({
           iconUrl={iconUrl}
           onTokenSelect={onTokenSelect}
         />
-        <input
-          type="number"
-          min={0}
+        <InputAmount
           disabled={isEmpty}
-          value={isEmpty ? '' : amount}
-          placeholder={isEmpty ? '' : placeholder}
-          className="field-input ms-2 flex-grow-1 text-end"
-          onChange={(event) => onAmountChange(event.target.value)}
+          amount={amount}
+          placeholder={placeholder}
+          onAmountChange={onAmountChange}
         />
       </div>
       <div className="d-flex justify-content-between mx-2">
-        <small>
-          {!isEmpty && `Balance: ${showBalance(token)}`}
-        </small>
-        <small>
-          {!isEmpty && price !== 0 && amount !== '' && `~$ ${(amo * price).toFixed(4)}`}
-        </small>
+        {children}
       </div>
     </div>
   );
 };
 
+const TokenAmountField = ({id, token, placeholder, onTokenSelect, onAmountChange}: TokenAmountFieldProps) => {
+  const {amount, price, isEmpty} = token;
+  const amo = parseFloat(amount);
+
+  return (
+    <TokenAmountFieldBase
+      id={id}
+      token={token}
+      placeholder={placeholder}
+      onTokenSelect={onTokenSelect}
+      onAmountChange={onAmountChange}
+    >
+      <small>
+        {!isEmpty && `Balance: ${showBalance(token)}`}
+      </small>
+      <small>
+        {!isEmpty && price !== 0 && amount !== '' && `~$ ${(amo * price).toFixed(4)}`}
+      </small>
+    </TokenAmountFieldBase>
+  );
+}
+
 export default TokenAmountField;
+
+
+export const TokenAmountFieldMax = ({id, token, placeholder, onTokenSelect, onAmountChange}: TokenAmountFieldProps) => {
+  const {amount, price, isEmpty} = token;
+  const amo = parseFloat(amount);
+
+  return (
+    <TokenAmountFieldBase
+      id={id}
+      token={token}
+      placeholder={placeholder}
+      onTokenSelect={onTokenSelect}
+      onAmountChange={onAmountChange}
+    >
+      <small>
+        {!isEmpty && `Balance: ${showBalance(token)}`}
+        {!isEmpty && <a className="text-primary text-decoration-none" onClick={() => onAmountChange(toBalance(token)+"")}>(Max)</a>}
+      </small>
+      <small>
+        {!isEmpty && price !== 0 && amount !== '' && `~$ ${(amo * price).toFixed(4)}`}
+      </small>
+    </TokenAmountFieldBase>
+  );
+}
+
+interface TokenAmountFieldImpactPriceProps extends TokenAmountFieldProps {
+  percentage: number;
+}
+
+const PercentageView = ({percentage}: {percentage: number}): JSX.Element => {
+  var color = "";
+  if (percentage > 0) { color = "text-success"; }
+  else if (percentage < 5) { color = "text-warning"; }
+  else { color = "text-danger"; }
+  return (
+    <span className={color}>({`${(percentage*100).toFixed(3)} %`})</span>
+  )
+}
+
+export const TokenAmountFieldImpactPrice = ({id, token, placeholder, percentage, onTokenSelect, onAmountChange}: TokenAmountFieldImpactPriceProps) => {
+  const {amount, price, isEmpty} = token;
+  const amo = parseFloat(amount);
+
+  const showUsd = !isEmpty && price !== 0 && amount !== '';
+
+  return (
+    <TokenAmountFieldBase
+      id={id}
+      token={token}
+      placeholder={placeholder}
+      onTokenSelect={onTokenSelect}
+      onAmountChange={onAmountChange}
+    >
+      <small>
+        {!isEmpty && `Balance: ${showBalance(token)}`}
+      </small>
+      <small>
+        {showUsd && `~$ ${(amo * price).toFixed(4)} `}
+        {showUsd && <PercentageView percentage={percentage} />}
+      </small>
+    </TokenAmountFieldBase>
+  );
+}
---
+++ src/components/inputs/Inputs.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+interface InputAmount {
+  amount: string;
+  disabled?: boolean;
+  placeholder?: string;
+  onAmountChange: (value: string) => void;
+}
+
+export const InputAmount = ({amount, onAmountChange, placeholder="", disabled=false} : InputAmount): JSX.Element => (
+  <input
+    type="number"
+    min={0}
+    disabled={disabled}
+    value={amount}
+    placeholder={placeholder}
+    className="field-input ms-2 flex-grow-1 text-end"
+    onChange={(event) => onAmountChange(event.target.value)}
+  />
+);
