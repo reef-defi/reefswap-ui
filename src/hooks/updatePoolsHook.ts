6d463c0d89f8d2aeb25600492c9eaff5d5ff01ff
Žiga Franko <franko.ziga@gmail.com>
2021-09-08 18:19:51 +0200

    few new hooks which will internally run state management

---
+++ src/hooks/updatePoolsHook.ts
@@ -0,0 +1,24 @@
+import { loadPools } from "../api/rpc/pools";
+import { loadingPools, setPools } from "../store/actions/pools";
+import { useAppDispatch, useAppSelector } from "../store/hooks"
+import { useAsyncEffect } from "./useAsyncEffect";
+
+export const updatePoolsHook = () => {
+  const dispatch = useAppDispatch();
+  const {tokens} = useAppSelector((state) => state.tokens);
+  const settings = useAppSelector((state) => state.settings);
+  const { accounts, selectedAccount} = useAppSelector((state) => state.accounts);
+
+  useAsyncEffect(async () => {
+    if (selectedAccount === -1) { return; }
+    const {signer} = accounts[selectedAccount];
+    await Promise.resolve()
+      .then(() => dispatch(loadingPools()))
+      .then(() => loadPools(tokens, signer, settings))
+      .then((res) => dispatch(setPools(res)))
+      .catch((error) => {
+        console.error(error)
+        dispatch(setPools([]));
+      })
+  }, [tokens]);
+}
---
+++ src/hooks/useAsyncEffect.ts
@@ -0,0 +1,7 @@
+import { useEffect } from "react"
+
+export const useAsyncEffect = (fun: () => Promise<void>, reference?: React.DependencyList): void => {
+  useEffect(() => {
+    fun();
+  }, reference)
+}
