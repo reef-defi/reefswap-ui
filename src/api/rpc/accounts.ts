b2f4c001c2f2816b9fdc645e4fc3d167c1ef42e9
Žiga Franko <franko.ziga@gmail.com>
2021-09-08 11:36:32 +0200

    extracting single account from injected accounts and rewriting accountsToSigners function with it

--- src/api/rpc/accounts.ts
+++ src/api/rpc/accounts.ts
@@ -4,18 +4,22 @@ import type { Signer as InjectedSigner } from '@polkadot/api/types';
 import { ReefswapSigner } from '../../store/actions/accounts';
 import { ensure } from '../../utils/utils';
 
+export const accountToSigner = async (account: InjectedAccountWithMeta, provider: Provider, sign: InjectedSigner): Promise<ReefswapSigner> => {
+  const signer = new Signer(provider, account.address, sign);
+  const evmAddress = await signer.getAddress();
+  const isEvmClaimed = await signer.isClaimed();
+
+  return {
+    signer,
+    evmAddress,
+    isEvmClaimed,
+    name: account.meta.name || '',
+    address: account.address
+  };
+}
+
 export const accountsToSigners = async (accounts: InjectedAccountWithMeta[], provider: Provider, sign: InjectedSigner): Promise<ReefswapSigner[]> => Promise.all(
-  accounts
-    .map((account) => ({
-      signer: new Signer(provider, account.address, sign),
-      name: account.meta.name || '',
-      address: account.address,
-    }))
-    .map(async (signer): Promise<ReefswapSigner> => ({
-      ...signer,
-      evmAddress: await signer.signer.getAddress(),
-      isEvmClaimed: await signer.signer.isClaimed(),
-    })),
+  accounts.map((account) => accountToSigner(account, provider, sign))
 );
 
 export const bindSigner = async (signer: Signer): Promise<void> => {
