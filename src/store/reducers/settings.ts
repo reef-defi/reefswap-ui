6a07db1c0edd14c7e1f620b58ac1972e7c8c0d1c
Ziga Franko G <frenkie@Zigas-MacBook-Pro-2.local>
2022-05-10 17:20:08 +0200

    fixes

--- src/store/reducers/settings.ts
+++ src/store/reducers/settings.ts
@@ -1,6 +1,6 @@
-import { ReefNetwork, reefNetworks } from '../../api/rpc/rpc';
-import { RELOAD_APP, SET_CHAIN_URL } from '../actionCreator';
-import { SettingsActions } from '../actions/settings';
+import { ReefNetwork, reefNetworks } from "../../api/rpc/rpc";
+import { RELOAD_APP, SET_CHAIN_URL } from "../actionCreator";
+import { SettingsActions } from "../actions/settings";
 
 export interface SettingsReducer extends ReefNetwork {
   reload: boolean;
@@ -8,13 +8,27 @@ export interface SettingsReducer extends ReefNetwork {
 
 const defaultSettings: SettingsReducer = {
   ...reefNetworks.mainnet,
+  graphqlUrl: reefNetworks.mainnet.graphqlUrl.replace("wss", "https"),
   reload: false,
 };
 
-export const settingsReducer = (state = defaultSettings, action: SettingsActions): SettingsReducer => {
+export const settingsReducer = (
+  state = defaultSettings,
+  action: SettingsActions
+): SettingsReducer => {
   switch (action.type) {
-    case RELOAD_APP: return { ...state, reload: !state.reload };
-    case SET_CHAIN_URL: return { ...state, ...reefNetworks[action.name] };
-    default: return state;
+    case RELOAD_APP:
+      return { ...state, reload: !state.reload };
+    case SET_CHAIN_URL:
+      return {
+        ...state,
+        ...reefNetworks[action.name],
+        graphqlUrl: reefNetworks[action.name].graphqlUrl.replace(
+          "wss",
+          "https"
+        ),
+      };
+    default:
+      return state;
   }
 };
